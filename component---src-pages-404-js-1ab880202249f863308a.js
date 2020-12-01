4395ee711bbcb7b5b2145847107c5cd378535660
Vaibhav Dwivedi <vaibhavforeverpr@gmail.com>
2020-12-01 17:50:23 +0530

    Updates

---
+++ component---src-pages-404-js-1ab880202249f863308a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{w2l6:function(t,n,c){"use strict";c.r(n);c("q1tI");var e=c("Bl7J"),u=c("vrFN"),o=c("qKvR");n.default=function(){return Object(o.c)(e.a,null,Object(o.c)(u.a,{title:"404: Not found"}),Object(o.c)("h1",null,"It's a 404!"),Object(o.c)("p",null,"You just hit a route that doesn't exist.."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-1ab880202249f863308a.js.map
